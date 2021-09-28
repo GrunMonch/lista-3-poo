70dec73648a60f2c40b5db6dea49da438992ec05
Irineu <irineo.rocha@gmail.com>
2021-09-27 23:40:13 -0300

    Created first classes

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,17 @@
+public class Pessoa {
+    public String nome;
+    public int cpf;
+
+    public void Pessoa(String nome, int cpf, int qtdEnd){
+
+    }
+
+    public void addEndereco(Endereco end){
+
+    }
+
+    public void mostraInfo(){
+
+    }
+
+}
